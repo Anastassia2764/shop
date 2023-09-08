0ee35ef81c26571d05ba340a03143dd5bc5b36e9
Anastassia2764 <a_reinsalu@online.ee>
2023-09-08 20:01:50 +0300

    Second testcommit

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 # this comment is only for commit, push
+# this comment is only for commit, push ---2!
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
